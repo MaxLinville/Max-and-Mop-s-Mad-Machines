a43aa148c5e77240ffb851f680186f7da52259be
MaxLinville <maxlinville7@gmail.com>
2022-10-09 12:55:26 -0400

    Added all of kaupenjoes files, plus functionality of SuperHopper block entity. Transfer cooldown for super hopper also added. Next steps are item stack transfer for super hopper, new textures, and more inventory slots.
    Next steps outside of hopper are add compressed blocks and new ores, remove kaupenjoes files from examplemod.

---
+++ src/main/java/net/kaupenjoe/tutorialmod/TutorialMod.java
@@ -0,0 +1,48 @@
+package net.kaupenjoe.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.kaupenjoe.tutorialmod.block.ModBlocks;
+import net.kaupenjoe.tutorialmod.block.entity.ModBlockEntities;
+import net.kaupenjoe.tutorialmod.event.PlayerTickHandler;
+import net.kaupenjoe.tutorialmod.fluid.ModFluids;
+import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.kaupenjoe.tutorialmod.networking.ModMessages;
+import net.kaupenjoe.tutorialmod.painting.ModPaintings;
+import net.kaupenjoe.tutorialmod.screen.ModScreenHandlers;
+import net.kaupenjoe.tutorialmod.util.ModLootTableModifiers;
+import net.kaupenjoe.tutorialmod.villager.ModVillagers;
+import net.kaupenjoe.tutorialmod.world.feature.ModConfiguredFeatures;
+import net.kaupenjoe.tutorialmod.world.gen.ModOreGeneration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModConfiguredFeatures.registerConfiguredFeatures();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+		ModVillagers.registerVillagers();
+		ModVillagers.registerTrades();
+
+		ModPaintings.registerPaintings();
+		ModOreGeneration.generateOres();
+
+		ModLootTableModifiers.modifyLootTables();
+		ModMessages.registerC2SPackets();
+
+		ModFluids.register();
+		ModBlockEntities.registerBlockEntities();
+
+		ModScreenHandlers.registerAllScreenHandlers();
+
+		ServerTickEvents.START_SERVER_TICK.register(new PlayerTickHandler());
+	}
+}
--- src/main/java/net/kaupenjoe/tutorialmod/mixin/ExampleMixin.java
+++ src/main/java/net/kaupenjoe/tutorialmod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.max.tutorialmod.mixin;
+package net.kaupenjoe.tutorialmod.mixin;
 
-import net.max.tutorialmod.TutorialMod;
+import net.kaupenjoe.tutorialmod.TutorialMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
